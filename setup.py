8c5cefb440a6bd8a3936bbc00876feb7658268fa
UnsettledAverage73 <145995913+atharvapatil1210@users.noreply.github.com>
2024-10-17 22:02:59 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='asm_py',
+    name='asm-chill-py',
     version='0.1.0',
     author='atharva bodade',
     author_email='atharvabodade@gmail.com',
